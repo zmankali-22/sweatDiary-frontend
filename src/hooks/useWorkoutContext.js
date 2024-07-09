dda2b1e9a289a3c9e38846a7956b66b4ca031426
acharya <sav.acharya22@gmail.com>
2024-07-09 13:11:40 +1000

    create useContext hook

---
+++ src/hooks/useWorkoutContext.js
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { WorkoutContext } from "../context/WorkoutContext"
+
+
+export const useWorkoutContext = () => {
+    const context = useContext(WorkoutContext)
+
+    if (!context) {
+        throw new Error("useWorkoutContext must be used within a WorkoutProvider")
+    }
+    return context;
+}
