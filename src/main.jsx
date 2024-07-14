5dedd6d93d14a99588285441ffa2717f33500c9c
acharya <sav.acharya22@gmail.com>
2024-07-14 10:25:30 +1000

    create context for login

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { WorkoutContextProvider } from './context/WorkoutContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { WorkoutContextProvider } from "./context/WorkoutContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-
-    <App />
-    </WorkoutContextProvider>
-  </React.StrictMode>,
-)
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
