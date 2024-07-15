7525e02d0e0527e6f1e862ed3fc271c102249541
acharya <sav.acharya22@gmail.com>
2024-07-15 20:55:45 +1000

    fix workout glitch

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutContext } from "./useWorkoutContext";
 
 export default function useLogout() {
   const { dispatch } = useAuthContext();
+  const {dispatch : workoutDispatch} = useWorkoutContext()
 
   const logout = () => {
     // remove user from storage
@@ -9,6 +11,7 @@ export default function useLogout() {
 
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
+    workoutDispatch({ type: "SET_WORKOUTS", payload: null });
   };
   return {logout};
 }
